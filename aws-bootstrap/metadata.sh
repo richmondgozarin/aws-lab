17e691ab95eebe2bfa22039e8a0c235a5120784e
Gozarin <ralph.r.v.gozarin@accenture.com>
2023-05-04 16:59:11 +0800

    AWS SAA Labs

---
+++ aws-bootstrap/metadata.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+yum update -y
+yum install httpd -y
+service httpd start
+cd /var/www/html
+echo "<html><body><h1>My IP is" > index.html 
+curl http://169.254.169.254/latest/meta-data/public-ipv4 >> index.html
+echo "</h1></body></html>" >> index.html 
